2dbff7662a99a547e5e118cb3f4edfff2e348043
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2024-03-08 23:21:52 +0000

    added items

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@
 import React from 'react';
 import Navigation from './component/navigation'; // Assuming you have the component in Example.js
 import Newsletter from './component/newsletter';
+import Shoppingcart from './component/shoppingcart';
+import Banner from './component/banner';
+import Product from './component/product';
 
 function App() {
   return (
 
   <> 
+    <Banner />
     <Navigation  
     />  
 
+    <Product />
 
+    <Shoppingcart  />
     <Newsletter />
 
     </>
