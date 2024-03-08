31e8f3011dee4e9e770f5884b85dde2295ac8dfe
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2024-03-08 22:21:29 +0000

    updated the navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+     
+      },
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif'],
+
+        'serif': ['Georgia', 'serif'],
+
+        'mono': ['ui-monospace', 'SFMono-Regular'],
+
+        'display': ['Oswald', 'sans-serif'],
+
+        'body': ['Inter', 'sans-serif'],
+
+        
+
+      },
+      colors: {
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+    },
+    
   },
-  plugins: [],
+  plugins: [
+    
+
+  ],
 }
+
+// tai
 
