7f377398a59e77054f212915bc447dfb50e53b71
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2024-03-13 19:27:39 +0000

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 // App.js
 
-import React from 'react'
-import Navigation from './component/navigation' // Assuming you have the component in Example.js
-import Newsletter from './component/newsletter'
-// import Shoppingcart from './component/shoppingcart';
-// import Banner from './component/banner'
-import Product from './component/product'
-import NewItem from './component/newItems'
-// import Cart from './component/cart'
+import React from 'react';
+import Navigation from './component/navigation';
+import Newsletter from './component/newsletter';
+// import Corousal from './component/corousal';
+import Product from './component/product';
+import NewItem from './component/newItems';
+
+
+
 
 function App() {
   return (
-    <>
-      {/* <Banner /> */}
+    <div className="app-container">
       <Navigation />
+      {/* <Corousal /> */}
       <NewItem />
+    
       <Product />
       <Newsletter />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
