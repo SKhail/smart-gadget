abcc2c8264688188c23bd1b64a7c455e2961a634
Rosemary Benson <rosebens@outlook.com>
2024-03-13 17:48:42 +0000

    connecting links

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // App.js
 
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Navigation from "./component/navigation"; // Assuming you have the component in Example.js
 import Newsletter from "./component/newsletter";
 // import Shoppingcart from './component/shoppingcart';
@@ -8,6 +9,13 @@ import Banner from "./component/banner";
 import Product from "./component/product";
 import NewItem from "./component/newItems";
 // import Cart from './component/cart'
+
+import React from 'react';
+import Home from './Home';
+import Consoles from './Consoles';
+import Laptops from './Laptops';
+import SmartPhones from './SmartPhones'; 
+import SpecialDeals from './SpecialDeals';
 
 function App() {
   return (
