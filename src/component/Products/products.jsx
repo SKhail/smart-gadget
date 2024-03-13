ce06ef9e2dfdd906f6b1e508807a38d5d7abc5d1
NadineTsianta <k.tsianta@rocketmail.com>
2024-03-13 19:17:34 +0000

    added code for laptop page && added component for products

---
+++ src/component/Products/products.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+ const products = [
+    {
+      id: 1,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    },
+    // More products...
+  ]
+
+  export default products
