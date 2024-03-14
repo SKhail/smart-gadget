910eb7298be93a2c5d00ae09d5347a3112a4670e
Said Khail <saidkhail@hotmail.com>
2024-03-14 19:27:05 +0000

    Fixed the npm packages errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // App.js
-import React, { useState } from 'react'
+import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Navigation from './component/navigation/Navbar'
 import Newsletter from './component/newsletter'
@@ -8,17 +8,20 @@ import Product from './component/product'
 import NewItem from './component/newItems'
 import LoginPage from './component/LoginFeature/LoginPage'
 // import Cart from './component/cart';
+import Corousal from './component/corousal'
 
 function App() {
   return (
     <BrowserRouter>
       <>
         <Navigation />
+        <Corousal />
         <NewItem />
         <Banner />
         <Product />
         <Newsletter />
       </>
     </BrowserRouter>
-
+  )
+}
 export default App
