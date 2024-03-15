36fb6f038d44c43f3c13517bddd2d33a137557de
NadineTsianta <k.tsianta@rocketmail.com>
2024-03-15 07:17:48 +0000

    fixed navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,33 @@ import Banner from '../src/component/banner/banner'
 import Product from './component/product'
 import NewItem from './component/newItems'
 import Corousal from '../src/component/corousal/index'
-import Home from '../src/component/home/Home'
-import Consoles from '../src/component/consoles/Consoles'
-import Laptops from '../src/component/laptops/Laptops'
-import SmartPhones from '../src/component/smartphones/SmartPhones'
-import SpecialDeals from '../src/component/specialdeals/SpecialDeals'
+
+import Home from "./component/home/Home";
+import Consoles from "./component/consoles/Consoles";
+import Laptops from "./component/laptops/Laptops";
+import SmartPhones from "./component/smartphones/SmartPhones";
+import SpecialDeals from "./component/specialdeals/SpecialDeals";
+
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path='/' exact component={Home} />
-          <Route path='/Consoles' component={Consoles} />
-          <Route path='/Laptops' component={Laptops} />
-          <Route path='/SmartPhones' component={SmartPhones} />
-          <Route path='/SpecialDeals' component={SpecialDeals} />
-        </Routes>
-        <Navigation />
-        <Corousal />
-        <NewItem />
-        <Banner />
-        <Product />
-        <Newsletter />
-      </div>
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Consoles" element={<Consoles />} />
+        <Route path="/Laptops" element={<Laptops />} />
+        <Route path="/SmartPhones" element={<SmartPhones />} />
+        <Route path="/SpecialDeals" element={<SpecialDeals />} />
+      </Routes>
+      <Corousal />
+      <NewItem />
+      <Banner />
+      <Product />
+      <Newsletter />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
