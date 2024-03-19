8be1ab15f817fa8a87c25adafc5b16586732edb2
Said Khail <saidkhail@hotmail.com>
2024-03-19 21:58:42 +0000

    Updated DarkMode File

--- src/App.jsx
+++ src/App.jsx
@@ -12,51 +12,43 @@ import Corousal from '../src/component/corousal/index'
 import Home from './component/home/Home'
 import Consoles from './component/consoles/Consoles'
 import Laptops from './component/Laptop/Laptop'
-
-
 import ShoppingCart from './component/cart/cart'
-
 import SmartPhones from './component/smartphones/SmartPhones'
 import SpecialDeals from './component/specialdeals/SpecialDeals'
 import { CardFooter } from '@material-tailwind/react'
 import QuickView from './component/quickview'
 
+//importing css
+import './index.css'
+
 //link to chat
-import ChatWindow from './component/chat/ChatWindow'; // Import from Chat folder
-import ChatInput from './component/chat/ChatInput'; // Import from Chat folder
-import Sidebar from './component/chat/sidebar'; // Import from Sidebar folder
-import ChatButton from './component/chat/ChatButton';
-import ChatSystem from '../src/component/chat/chatsystem'; 
-
-
+import ChatWindow from './component/chat/ChatWindow' // Import from Chat folder
+import ChatInput from './component/chat/ChatInput' // Import from Chat folder
+import Sidebar from './component/chat/sidebar' // Import from Sidebar folder
+import ChatButton from './component/chat/ChatButton'
+import ChatSystem from '../src/component/chat/chatsystem'
 
 function App() {
   const [cart, setCart] = useState([])
   const [darkMode, setDarkMode] = useState(false) // Handle Dark Mode state
   const [showModal, setShowModal] = useState(false) // Login Page
 
-  const [showChatSystem, setShowChatSystem] = useState(false);
-  const [messages, setMessages] = useState([]);
+  const [showChatSystem, setShowChatSystem] = useState(false)
+  const [messages, setMessages] = useState([])
 
   const handleChatButtonClick = () => {
-    setShowChatSystem(true);
-  };
-
-  const handleCloseChat = () => {
-    setShowChatSystem(false);
-  };
-
-  const handleSendMessage = (text) => {
-    setMessages([...messages, { sender: 'Me', text }]);
-  };
-  const addtoCart = (item) => {
-    setCart([...cart, item])
+    setShowChatSystem(true)
   }
 
-  // Helper function to get initial mode
-  function getInitialMode() {
-    const savedMode = JSON.parse(localStorage.getItem('darkMode'))
-    return savedMode || false // Default to false if no mode is saved
+  const handleCloseChat = () => {
+    setShowChatSystem(false)
+  }
+
+  const handleSendMessage = (text) => {
+    setMessages([...messages, { sender: 'Me', text }])
+  }
+  const addtoCart = (item) => {
+    setCart([...cart, item])
   }
 
   // Used for the Login Page
@@ -67,39 +59,35 @@ function App() {
   // toggle dark mode
   const toggleDarkMode = () => {
     setDarkMode((prevMode) => !prevMode)
+    localStorage.setItem('darkMode', !darkMode) // Save dark mode state in localStorage
   }
 
   return (
     <Router>
-      <Navigation darkMode={darkMode} toggleDarkMode={toggleDarkMode} toggleLoginPage={() => setShowModal(!showModal)} showModal={showModal} />
+      <div className={`app ${darkMode ? 'dark-mode' : ''}`}>
+        <Navigation darkMode={darkMode} toggleDarkMode={toggleDarkMode} toggleLoginPage={() => setShowModal(!showModal)} showModal={showModal} />
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/consoles' element={<Consoles />} />
-        <Route path='/laptops' element={<Laptops addtoCart={addtoCart} />} />
-        <Route path='/shoppingcart' element={<ShoppingCart cart={cart} />} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/consoles' element={<Consoles />} />
+          <Route path='/laptops' element={<Laptops addtoCart={addtoCart} />} />
+          <Route path='/shoppingcart' element={<ShoppingCart cart={cart} />} />
 
-        <Route path='/smartphones' element={<SmartPhones addtoCart={addtoCart} />} />
-        <Route path='/specialdeals' element={<SpecialDeals />} />
-      </Routes>
+          <Route path='/smartphones' element={<SmartPhones addtoCart={addtoCart} />} />
+          <Route path='/specialdeals' element={<SpecialDeals />} />
+        </Routes>
 
-      <div className="app">
-      {!showChatSystem && <ChatButton onClick={handleChatButtonClick} />}
-      {showChatSystem && (
-        <ChatSystem
-          messages={messages}
-          onSendMessage={handleSendMessage}
-          onClose={handleCloseChat}
-        />
-      )}
-    </div>
-
-      <QuickView />
-      <NewItem />
-      <Corousal />
-      <Banner />
-      <Product />
-      <Newsletter />
+        <div className='app'>
+          {!showChatSystem && <ChatButton onClick={handleChatButtonClick} />}
+          {showChatSystem && <ChatSystem messages={messages} onSendMessage={handleSendMessage} onClose={handleCloseChat} />}
+        </div>
+        <QuickView />
+        <NewItem />
+        <Corousal />
+        <Banner />
+        <Product />
+        <Newsletter />
+      </div>
     </Router>
   )
 }
