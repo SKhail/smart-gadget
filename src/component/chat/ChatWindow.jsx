5d88986b18fbd48198a3a8f6606522eba943c456
Said Khail <saidkhail@hotmail.com>
2024-03-22 16:13:19 +0000

    Added the message in the WebChat

--- src/component/chat/ChatWindow.jsx
+++ src/component/chat/ChatWindow.jsx
@@ -1,24 +1,33 @@
-
-import React from 'react';
-import Message from './Message';
+import React from 'react'
+import Message from './Message'
 
 //import css
-import './style.css';
-
+import './style.css'
 
 const ChatWindow = ({ messages }) => {
-    // Check if messages is undefined or null
-    if (!messages) {
-      return <div>No messages available</div>;
-    }
-  
-    return (
-      <div className="chat-window">
-        {messages.map((message, index) => (
-          <Message key={index} message={message} />
-        ))}
-      </div>
-    );
-  };
+  // Check if messages is undefined or null
+  if (!messages) {
+    return <div>No messages available</div>
+  }
 
-  export default ChatWindow;
+  // Function to check if a welcome message should be displayed
+  const shouldDisplayWelcomeMessage = () => {
+    // Check if there are no messages or if the first message is not from the user
+    return !messages.length || messages[0].sender !== 'user'
+  }
+
+  // Render the chat window with messages
+  return (
+    <div className='chat-window'>
+      {/* Render the welcome message if necessary */}
+      {shouldDisplayWelcomeMessage() && <Message key='welcome' message={{ sender: 'system', text: 'Welcome to Smart Gadget! How can we assist you today?' }} />}
+
+      {/* Render each message */}
+      {messages.map((message, index) => (
+        <Message key={index} message={message} />
+      ))}
+    </div>
+  )
+}
+
+export default ChatWindow
