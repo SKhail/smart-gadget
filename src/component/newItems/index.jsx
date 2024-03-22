b727217c940c321ff1db071cf038f2b3ca32dbe8
Medj41 <larbi.djouama@gmail.com>
2024-03-22 15:43:21 +0000

    added shop selection

--- src/component/newItems/index.jsx
+++ src/component/newItems/index.jsx
@@ -78,7 +78,7 @@ export default function Example({ darkMode }) {
                   </div>
                 </div>
               </div>
-              <Link to="/favoritegadgets" >
+              <Link to="/favoritegadget" >
                  <button className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700">Shop Collection</button>
               </Link>
             </div>
